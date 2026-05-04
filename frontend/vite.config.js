8b7843380d020580888534261be5042f4c1d5d78
Radheshyam Verma <166715325+Radhe127@users.noreply.github.com>
2026-05-04 21:28:30 +0000

    Update Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://20.219.136.125:8080',
         changeOrigin: true,
       }
     }
